04b083a2991b37a5a9bfdc0bc777b51acec8b001
Luis <luis.mogueadp@gmail.com>
2022-04-24 11:38:37 -0500

    Se agrega proyecto

---
+++ index.js
@@ -0,0 +1,8 @@
+var  number1 = 17;
+var  number2  = 8;
+
+if(number1 <= 8 ) {
+    console.log(`${number1} isn't greater than ${number2}`)
+} else {
+    console.log(`${number1} is greater than ${number2}`)
+}
